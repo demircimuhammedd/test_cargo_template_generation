183638ab1f11b7372ba0804252c75ce5ce45c114
demircimuhammedd <demirci.muhammedd@gmail.com>
2022-09-19 09:47:57 +0300

    test

---
+++ template2/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Hello, {{authors}}!");
+    println!("The boolean was: {{some_bool}}");
+    println!("Your choice was: {{choice}}");
+    println!("Hello, {{authors}}!");
+    println!("The boolean was: {{some_bool}}");
+    println!("Your choice was: {{choice}}");
+}
