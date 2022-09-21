01dcf48da1cac5cf188d8a03112d74a94ce6f4ca
demircimuhammedd <demirci.muhammedd@gmail.com>
2022-09-21 07:58:38 +0300

    added names

--- template2/src/main.rs
+++ template2/src/main.rs
@@ -1,4 +1,7 @@
 fn main() {
+    println!("Hello, {{crate_name}}!");
+    println!("Hello, {{project_name}}!");
+    println!("Hello, {{name}}!");
     println!("Hello, {{authors}}!");
     println!("The boolean was: {{some_bool}}");
     println!("Your choice was: {{choice}}");
